4d94993372dff661287638aa3f3704ce3ee0f6a0
Rachit03 <30517432+Rachit03@users.noreply.github.com>
2025-09-07 18:43:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from streamlit_javascript import st_javascript
 
+st.set_page_config(page_title="Get Client IP", layout="centered")
 st.title("Client IP Address")
 
-ip = st_javascript("await fetch('https://api.ipify.org?format=json').then(res => res.json()).then(data => data.ip)")
-st.write("Your IP Address is:", ip)
+with st.spinner("Fetching your IP address..."):
+    ip = st_javascript("await fetch('https://api.ipify.org?format=json').then(res => res.json()).then(data => data.ip)")
+
+if ip:
+    st.success(f"Your IP Address is: {ip}")
+else:
+    st.error("Could not fetch your IP address.")
